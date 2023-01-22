34f2b4f6132b33a0c77ecb22d58aeb8a794d27c5
Travier Moorlag <moorlagt@gmail.com>
2023-01-21 20:46:03 -0500

    working on getting review apps to boot

--- orchestrator/main.go
+++ orchestrator/main.go
@@ -3,8 +3,6 @@ package main
 import (
 	"fmt"
 	"reviewhub-cli/orchestrator/core"
-	"reviewhub-cli/orchestrator/docker_engine"
-	"reviewhub-cli/orchestrator/git_repo"
 	"reviewhub-cli/orchestrator/webhooks"
 )
 
@@ -21,23 +19,23 @@ func main() {
 	logger.Info().Msg(fmt.Sprintf("Starting Webhook Server %s", webhookAddress))
 	webhooks.StartHttpServer(webhookAddress)
 
-	storedRepoInfo := git_repo.GetRepo(git_repo.RepoInfo{
-		Owner:  "verticelabs-dev",
-		Name:   "reviewhub-example-app",
-		Branch: "main",
-	})
+	// storedRepoInfo := git_repo.GetRepo(git_repo.RepoInfo{
+	// 	Owner:  "verticelabs-dev",
+	// 	Name:   "reviewhub-example-app",
+	// 	Branch: "main",
+	// })
 
-	containerStartConfig := docker_engine.ContainerStartConfig{
-		ContainerName: fmt.Sprintf("%s-%s", storedRepoInfo.Name, storedRepoInfo.Branch),
-		ImageName:     storedRepoInfo.ImageName,
-		ExposedPort:   8080,
-		HostIP:        "0.0.0.0",
-		HostPort:      4040,
-	}
+	// containerStartConfig := docker_engine.ContainerStartConfig{
+	// 	ContainerName: fmt.Sprintf("%s-%s", storedRepoInfo.Name, storedRepoInfo.Branch),
+	// 	ImageName:     storedRepoInfo.ImageName,
+	// 	ExposedPort:   8080,
+	// 	HostIP:        "0.0.0.0",
+	// 	HostPort:      4040,
+	// }
 
-	logger.Info().Msg(fmt.Sprintf("Container name is %s", containerStartConfig.ContainerName))
-	docker_engine.ContainerDestroyByName(containerStartConfig.ContainerName)
+	// logger.Info().Msg(fmt.Sprintf("Container name is %s", containerStartConfig.ContainerName))
+	// docker_engine.ContainerDestroyByName(containerStartConfig.ContainerName)
 
-	git_repo.BuildRepoImage(storedRepoInfo)
-	docker_engine.ContainerStart(containerStartConfig)
+	// git_repo.BuildRepoImage(storedRepoInfo)
+	// docker_engine.ContainerStart(containerStartConfig)
 }
--- orchestrator/webhooks/webhooks.go
+++ orchestrator/webhooks/webhooks.go
@@ -5,14 +5,19 @@ import (
 	"fmt"
 	"net/http"
 	"reviewhub-cli/orchestrator/core"
+	"reviewhub-cli/orchestrator/docker_engine"
+	"reviewhub-cli/orchestrator/git_repo"
+	"strings"
 )
 
 type RequestData struct {
-	Branch string `json:"branch"`
+	Branch string
+	Repo   string
 }
 
 func handleBranchAction(w http.ResponseWriter, r *http.Request) {
 	logger := core.GetLogger()
+	logger.Info().Msg("Webhook Request Received")
 
 	if r.Method != http.MethodPost {
 		w.WriteHeader(http.StatusMethodNotAllowed)
@@ -20,13 +25,40 @@ func handleBranchAction(w http.ResponseWriter, r *http.Request) {
 	}
 
 	var data RequestData
-	decoder := json.NewDecoder(r.Body)
-	err := decoder.Decode(&data)
+	err := json.NewDecoder(r.Body).Decode(&data)
 	if err != nil {
+		logger.Error().Msg(err.Error())
+
 		w.WriteHeader(http.StatusBadRequest)
 		fmt.Fprint(w, "Invalid JSON data")
 		return
 	}
+
+	logger.Info().Msg(fmt.Sprintf("Webhook Request for Repo %s and Branch %s", data.Repo, data.Branch))
+	repoData := strings.Split(data.Repo, "/")
+	owner := repoData[0]
+	name := repoData[1]
+	branch := data.Branch
+
+	storedRepoInfo := git_repo.GetRepo(git_repo.RepoInfo{
+		Owner:  owner,
+		Name:   name,
+		Branch: branch,
+	})
+
+	containerStartConfig := docker_engine.ContainerStartConfig{
+		ContainerName: fmt.Sprintf("%s-%s", storedRepoInfo.Name, storedRepoInfo.Branch),
+		ImageName:     storedRepoInfo.ImageName,
+		ExposedPort:   8080,
+		HostIP:        "0.0.0.0",
+		HostPort:      4040,
+	}
+
+	logger.Info().Msg(fmt.Sprintf("Container name is %s", containerStartConfig.ContainerName))
+	docker_engine.ContainerDestroyByName(containerStartConfig.ContainerName)
+
+	git_repo.BuildRepoImage(storedRepoInfo)
+	docker_engine.ContainerStart(containerStartConfig)
 
 	logger.Info().Msg(fmt.Sprintf("Webhook Request for Branch %s", data.Branch))
 	fmt.Fprint(w, "Thanks!")
